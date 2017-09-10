11e81c936cc702d64ffb5f366744d9bb2871ddd7
Czyryca <k.czyryca@gmail.com>
2017-09-10 16:14:56 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tweet-generator
-Builds tweets using Markov Chains
+Python 2 script that builds tweets using Markov Chains. Connects to the Twitter Firehose, learns which words follow which, then strings a valid tweet together. Refuses to use profanity, hyperlinks, or 141+ characters at a time.
+Usage: python listener.py <subject>; sh run.sh <subject> <num of tweets>
+If you want to do one step at a time:
+Usage: python listener.py <subject>; python buildmarkov.py <subject>; python tweetbuilder.py <num of tweets>
--- listener.py
+++ listener.py
@@ -7,22 +7,20 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+#ADD YOUR STUFF HERE!
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
-# ADD YOUR STUFF HERE!
-consumer_key = "G5MunMpwiuaS9ZMsz1AjOtsck"
-consumer_secret = "2u6RjLHAFmGXxFvxNeSHYFN6zJK101OvQs1sOYDhgQghM1To1h"
-access_token = '4899753597-wYH1V2Cld7lEdUKiMCEAtjtxk5IfxdjTnED6BXl'
-access_token_secret = 'DDUc2FL4uUSPeiJqpfUN9iqFhqM3Hi9rsQsu5b9HsvcbU'
 
-
-#count = 0
 class MyStreamListener(StreamListener):
     """ 
     A listener handles tweets that are received from the stream.
     """
     def on_data(self, dataStr):
         data = json.loads(dataStr)
-        if "text" not in data:#"empty" tweet? ignore
+        if "text" not in data:#"empty tweet? ignore
             return True
         print data["text"]
         output.write(data["text"].encode('utf-8')+'\n')
@@ -42,9 +40,9 @@ if __name__ == "__main__":
     output_path = topic+'.txt'
     output = open(output_path,'a')
 
-    l = MyStreamListener()
+    listen = MyStreamListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
+    stream = Stream(auth, listen)
     stream.filter(track=[topic])
 
